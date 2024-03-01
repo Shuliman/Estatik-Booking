ce9673490171f81e85c34465ef4a8f0215e48892
Shuliman <Julik.schuldiner@proton.me>
2024-03-01 17:39:46 +0100

    init

---
+++ estatik-bookings.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Plugin Name: Estatik Bookings
+Description: Custom Booking System for WordPress.
+Version: 1.0
+Author: Shuliman
+*/
+
+// Registration of the custom record type 'booking'
+function estatik_register_booking_cpt()
+{
+    $args = array(
+        'public' => true,
+        'label' => 'Bookings',
+        'supports' => array('title', 'editor', 'custom-fields'),
+    );
+    register_post_type('booking', $args);
+}
+
+add_action('init', 'estatik_register_booking_cpt');
+
+function estatik_bookings_activate()
+{
+    estatik_register_booking_cpt();
+    flush_rewrite_rules();
+}
+
+register_activation_hook(__FILE__, 'estatik_bookings_activate');
+
+
+function estatik_bookings_deactivate()
+{
+    // Resetting overwrite rules
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook(__FILE__, 'estatik_bookings_deactivate');
